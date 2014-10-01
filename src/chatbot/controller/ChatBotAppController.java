df122729f67d456258f51a0d9e0afb32d5b646a4
AndrewWilleitner <andrewwilleitner@gmail.com>
2014-10-01 09:34:43 -0600

    added memes
    added meme array

--- src/chatbot/controller/ChatBotAppController.java
+++ src/chatbot/controller/ChatBotAppController.java
@@ -4,7 +4,7 @@ package chatbot.controller;
 import chatbot.model.Chatbot;
 import chatbot.view.ChatbotView;
 /**
- * 
+ * Application Controller for the Chatbot String manipulation project. Responsible for controlling the View and Model packages.
  * @author Andrew Willeitner
  * @version 0.1 10/1/14
  */
@@ -14,8 +14,9 @@ public class ChatBotAppController
 	 * The GUI view for the application
 	 */
 	private ChatbotView appView;
+	
 	/**
-	 * 
+	 * The Chatbot model instance.
 	 */
 	private Chatbot notSoCleverBot;
 	
@@ -27,14 +28,12 @@ public class ChatBotAppController
 	/**
 	 * Creates a ChatbotAppController and initializes the associated View and Model objects.
 	 */
-	
 	public ChatBotAppController()
 	{
 		appView = new ChatbotView(this);
 		notSoCleverBot = new Chatbot("Mr. not so clever");
 		startMessage = "Welcome to the " + notSoCleverBot.getName() + " Chatbot, type in your name.";
 	}
-	
 	
 	/**
 	 * Allows other objects access to the notSoCleverbot.
@@ -48,7 +47,6 @@ public class ChatBotAppController
 	/**
 	 * Start the Chatbot Application.
 	 */
-	
 	public void start()
 	{
 		String message = appView.displayChatbotConversations(startMessage);
@@ -56,9 +54,11 @@ public class ChatBotAppController
 		// ! means not
 		while(!notSoCleverBot.quitChecker(message))
 		{
+			message = notSoCleverBot.processText(message);
 			message = appView.displayChatbotConversations(message);
 		}
-
+		
+		quit();
 	}
 	
 	/**
--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -10,7 +10,7 @@ public class Chatbot
 	
 	
 	/**
-	 * Creates a Chatbot object with a specified name. Initializes the total chats to 0.
+	 * Creates a Chatbot object with a specified name. Initializes the total chats to 0. Creates and fills the memeList.
 	 * @param name The name of the chatbot.
 	 */
 	public Chatbot(String name)
@@ -18,6 +18,8 @@ public class Chatbot
 		this.name = name;
 		numberOfChats = 0;
 		// this. means talk to the current class
+		memeList = new ArrayList<String>();
+		fillTheMemeList();
 	}
 
 	public String getName()
@@ -47,6 +49,57 @@ public class Chatbot
 	{
 		numberOfChats++;
 	}
+	
+	/**
+	 * fills the contents of the memeList.
+	 */
+	private void fillTheMemeList()
+	{
+		memeList.add("U mad bro?");
+		memeList.add("WUD!");
+		memeList.add("NO.");
+		memeList.add("Ain't nobody got time for dat!");
+		memeList.add("Y U DO THIS");
+		memeList.add("This is why we can't have nice things.");
+	}
+	
+	/**
+	 * Processed the supplied text from the user to provide a message from the Chatbot.
+	 * @param userText The user supplied text.
+	 * @return What the Chatbot says because of the supplied input.
+	 */
+	public String processText(String userText)
+	{
+		String processedText = "";
+		incrementChats();
+		
+		if(memeChecker(userText))
+		{
+			processedText = "hey, you found a meme: " + userText;
+			processedText += " isn't that cool.";
+		}
+		else
+		{
+			processedText = "Boring, that wasn't a meme";
+		}
+		return processedText;
+	}
+	
+	private boolean memeChecker(String currentText)
+	{
+		boolean isAMeme = false;
+		
+		for(String currentMeme : memeList)
+		{
+			if(currentMeme.equalsIgnoreCase(currentText))
+			{
+				isAMeme = true;
+			}
+		}
+		
+		return isAMeme;
+	}
+	
 	/**
 	 * checks for input to close app.
 	 * @param input
